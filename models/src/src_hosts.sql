6ea5abdc26ed21a5632bc729c548c9acb4a70d76
samuelzhang <samuelzhang.ym@gmail.com>
2022-10-16 21:14:45 +1100

    change materialization

--- models/src/src_hosts.sql
+++ models/src/src_hosts.sql
@@ -1,14 +1,14 @@
 WITH raw_hosts AS (
-    SELECT
-        *
-    FROM
-        AIRBNB.RAW.RAW_HOSTS
+SELECT
+*
+FROM
+AIRBNB.RAW.RAW_HOSTS
 )
 SELECT
-    id AS host_id,
-    NAME AS host_name,
-    is_superhost,
-    created_at,
-    updated_at
+id AS host_id,
+NAME AS host_name,
+is_superhost,
+created_at,
+updated_at
 FROM
-    raw_hosts
+raw_hosts
